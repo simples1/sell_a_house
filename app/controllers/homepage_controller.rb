31b1f1c0f4e60d7a9f3d020d59d2776e745eceaa
simples1 <abdullaaden@outlook.com>
2017-11-16 23:39:54 +0000

    adds smoke test for homepage

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,7 @@
+class HomepageController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def home
+
+  end
+end
