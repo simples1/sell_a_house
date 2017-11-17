ef237ab32fb8eea1d60bd247200b09d86e660226
simples1 <abdullaaden@outlook.com>
2017-11-17 23:59:09 +0000

    adds first test for Users modal

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "can create Users" do
+		before do
+			@user = User.create(email: "ww@ww.com", first_name: "jj", last_name: "pp", password: "123456789")
+			@user2 = User.create(email: "ww@ww.com", password: "123456789")
+		end
+
+		it "can be created" do
+			expect(@user).to be_valid
+		end
+
+		it "validate first_name and last_name when creating user" do
+			expect(@user2).to_not be_valid
+		end
+	end
 end
